32e3cbb14a815d48b128cd2d2e6445a73d659e7e
Dan Paz <dan@daniel-paz.com>
2022-10-29 22:48:26 -0500

    Add args for webserver container and include basic PHP page at doc root

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>HTML 5 Boilerplate</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <!-- https://www.freecodecamp.org/news/basic-html5-template-boilerplate-code-example/ -->
+    <h1>Hello World.</h1>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/RH1ekuvSYzE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  </body>
+</html>
